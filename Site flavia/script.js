212aa04f71f985f0ad27373719a7c512060685cc
Leonardo Sousa <leofsousa98@gmail.com>
2023-10-19 23:24:40 -0300

    Criei o hiddem Menu

---
+++ Site flavia/script.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+    document.getElementById("menu").style.width="250px";
+    document.getElementById("principal").style.marginLeft="250px";
+}
+
+function fecharNav(){
+    document.getElementById("menu").style.width="0";
+    document.getElementById("principal").style.marginLeft="0";
+}
